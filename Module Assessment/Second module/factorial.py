03a0b4827494fa0b17374bb8d6ea2d74f68d86bd
Kelly0672 <169105220+Kelly0672@users.noreply.github.com>
2024-06-02 14:23:24 -0400

    Add files via upload

---
+++ Module Assessment/Second module/factorial.py
@@ -0,0 +1,18 @@
+import math
+num= input("Enter an integer number") 
+num= int(num)
+
+
+ 
+if num <=1:
+    print ("1")
+       
+        
+else:       
+    a = math.factorial (num)
+    print(a)
+  
+    
+   
+
+ 
